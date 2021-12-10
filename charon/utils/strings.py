dd99125d5e80d50c6d6e6b853e687638b480a210
shokakucarrier <liyu@redhat.com>
2021-12-10 15:15:59 +0800

    removed unused function

--- charon/utils/strings.py
+++ charon/utils/strings.py
@@ -2,8 +2,3 @@ def remove_prefix(string: str, prefix: str) -> str:
     if string and prefix and string.startswith(prefix):
         return string[len(prefix):]
     return string
-
-def remove_sufix(string: str, sufix: str) -> str:
-    if string and sufix and string.endswith(sufix):
-        return string[:-len(sufix)]
-    return string
