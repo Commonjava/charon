f463d15291573819c46a5c9d90e98bbe21341c60
Gang Li <ligangty@users.noreply.github.com>
2022-08-17 17:16:17 +0800

    Fix: Remove boto3_type_annotations from requirements due to rpm deps issue

--- tests/test_s3client.py
+++ tests/test_s3client.py
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 from typing import List
-from boto3_type_annotations import s3
 from charon.storage import S3Client, CHECKSUM_META_KEY
 from charon.utils.archive import extract_zip_all
 from charon.utils.files import overwrite_file, read_sha1
@@ -153,7 +152,7 @@ class S3ClientTest(BaseTest):
             product="apache-commons", root=root
         )
 
-        def content_check(products: List[str], objs: List[s3.ObjectSummary]):
+        def content_check(products: List[str], objs: List):
             self.assertEqual(COMMONS_LANG3_ZIP_ENTRY, len(objs))
             for o in objs:
                 obj = o.Object()
